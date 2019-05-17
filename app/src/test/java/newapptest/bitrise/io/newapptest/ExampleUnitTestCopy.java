224ac839aa82b97626a807481c2d8fe16d407e30
Papik Tamas <trapacska@gmail.com>
2019-05-17 15:53:25 +0200

    initial

---
+++ app/src/test/java/newapptest/bitrise/io/newapptest/ExampleUnitTestCopy.java
@@ -0,0 +1,17 @@
+package newapptest.bitrise.io.newapptest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTestCopy {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
